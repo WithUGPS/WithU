f2c72e95e40442800956efacf00d10441dd4c6e7
WithUGPS <100690835+WithUGPS@users.noreply.github.com>
2022-03-05 00:05:43 +0100

    Add files via upload

--- Prototipo/app.js
+++ Prototipo/app.js
@@ -1,32 +1,27 @@
 "use strict";
 
-//const fs = require('fs');
 const DAOPersonas = require('./public/javascripts/daoPersonas')
-
 const path = require("path");
 const express = require("express");
 const app = express();
-
-//const bodyParser = require("body-parser");
-
+const multer = require("multer");
+const bodyParser = require('body-parser');
 const config = require("./config");
-
 const mysql = require("mysql");
+
 const pool = mysql.createPool({
 	host: config.host,
 	user: config.user,
 	password: config.password,
 	database: config.database
 });
+const multerFactory = multer({ storage: multer.memoryStorage() });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
-//app.use(bodyParser.urlencoded({ extended: true }));
-
-//let usuarios = [];
-//let id = 1;
+app.use(bodyParser.urlencoded({ extended: true }));
 
 function cb_getAll(err, us){
 	if(err){
@@ -55,12 +50,12 @@ app.get("/", function(request, response){
 
 app.get("/start", function(req,res){
 	let daoP = new DAOPersonas(pool);
-	daoP.getAll(cb_getAll);
+	daoP.getPersonas(cb_getAll);
 });
 
-app.post("/anadirPersona", function(req, res){
-	let persona = req.body.persona;
+app.post("/anadirPersona", [middle1, middle2], function(req, res){
 	let dao = new DAOPersonas(pool);
+	let persona = req.body.persona;
 	dao.addPersona(persona, cb_addPersona);
 });
 
